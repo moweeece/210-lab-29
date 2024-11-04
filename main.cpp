8c98b03d31236689886104b2313e32c41ac97e55
moweeece <mjespinosa990@gmail.com>
2024-11-03 22:25:46 -0800

    2nd Commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// COMSC-210 | Mauricio Espinosa | Lab 29 Project
+// IDE Used: Visual Studio Code & Github
+
+//include necessary headers 
+#include <fstream>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+// function declarations
+// simulation function
+
+
+// main function
+int main() {
+    // seed random number generator
+    // variable declarations
+    // initialize a robot map
+
+    // open external file
+    // randomly read robot IDs from file and assign to robot std::map
+    // initialize robot task list
+    // close the file
+
+    // call function to simulate robot tasks over a certain time period
+
+    // display final state of robots
+
+    return 0;
+}
+
+// function definitions
+// simulation function
+    // randomly assign tasks to each robot
